330dfae9a491ccf99b82e1ca61729e1a6a0ad83e
Andrew White <andyn@ANDREW-9V5QOQ8F.White.local>
2022-11-13 13:01:57 -0500

    RE:#10 added in checking for url exact todo change to regex

--- src/main/java/LocalClient/LocalClient.java
+++ src/main/java/LocalClient/LocalClient.java
@@ -1,7 +1,10 @@
 package LocalClient;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,16 +17,29 @@ public class LocalClient {
 
 	public static void run() {
 		String baseUrl = "https://google.com";
+		
+		run(baseUrl, "about");
+	}
+	
+	//TODO determine if that can be a static
+	public static void run (String start_uri, String end_uri_regex) {
 
 		WebDriverManager.firefoxdriver().setup();
 		WebDriver myWebDriver = new FirefoxDriver();
-		myWebDriver.get(baseUrl);
+		myWebDriver.get(start_uri);
 		
 		//Opened now check it
 		logger.info(myWebDriver.getCurrentUrl());
 
 		// get the actual value of the title
 		logger.info(myWebDriver.getTitle());
+		
+		//TODO change to regex
+		 boolean resultedInURI = new WebDriverWait(myWebDriver, Duration.ofSeconds(3))
+        .until(driver -> (myWebDriver.getCurrentUrl().contains(end_uri_regex) ) );
+		 
+		 //If user hits the url we get this to be true
+		 logger.info(Boolean.toString(resultedInURI));
 	}
 
 }
